2e63890bb11766fdd3f98be0228bc69489b4c1d8
Walker-AVC <3146008238@qq.com>
2022-09-21 20:49:22 +0800

    ccs

--- Class1/Works/Advance/3/3.cpp
+++ Class1/Works/Advance/3/3.cpp
@@ -2,6 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int A (int x)
+{
+    if (x<0)
+    {
+        x=-x;
+    }
+    return x;
+}
+
 int main()
 {
     int h1, h2, h3, m1, m2, m3, s1, s2, s3, t;
@@ -10,15 +19,17 @@ int main()
     printf("时刻2:___时___分___秒\n");
     scanf("%d%d%d", &h2 , &m2 , &s2 );
     h3 = h1 - h2, m3 = m1 - m2, s3 = s1 - s2;
-    
-    if (h3 < 0)
-        {h3 = -h3;}
+    A (h3);
+    A (m3);
+    A (s3);
+    //  if (h3 < 0)
+    //     {h3 = -h3;}
 
-    if (m3 < 0)
-        {m3 = -m3;}
+    //  if (m3 < 0)
+    //      {m3 = -m3;}
 
-    if (s3 < 0)
-        {s3 = -s3;}
+    // if (s3 < 0)
+    //     {s3 = -s3;}
 
     t=h3*3600+m3*60+s3;
     printf("这两个时刻相差%d",t);
